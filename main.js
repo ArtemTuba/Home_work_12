cf9b72d7e7fadc8b85cfe3b7851c1f3c5736352c
ArtemTuba <tuba9521@gmail.com>
2022-12-20 13:05:14 +0200

    Add index check

--- main.js
+++ main.js
@@ -2,9 +2,13 @@ const array = [1, 2, 3, 4, 5, 6, 7];
 
 function removeElement(array, item){
     const element = array.indexOf(item);
-    array.splice(element,1);
+    if(item !== -1){
+        array.splice(element, 1);
+        console.log(array);
+    } else {
+        console.log('Tакого елемента немає в масиві');
+    }
     return array;
 }
 
 removeElement(array, 5);
-console.log(array)
