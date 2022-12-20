4091e4b663241d898874adab2c79758f9491ed73
ArtemTuba <tuba9521@gmail.com>
2022-12-20 15:43:58 +0200

    Fixed home work

--- main.js
+++ main.js
@@ -2,13 +2,14 @@ const array = [1, 2, 3, 4, 5, 6, 7];
 
 function removeElement(array, item){
     const element = array.indexOf(item);
-    if(item !== -1){
+    if(array.indexOf(item) === -1){
+        console.log('Tакого елемента немає в масиві');
+    } else {
         array.splice(element, 1);
         console.log(array);
-    } else {
-        console.log('Tакого елемента немає в масиві');
     }
     return array;
 }
 
 removeElement(array, 5);
+
